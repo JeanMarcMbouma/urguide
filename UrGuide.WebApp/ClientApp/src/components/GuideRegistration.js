2d6c4c5f2bbc54fe526295203e918f2da6aec163
Jean Marc Mbouma <jeanmarcm@instem.com>
2020-03-18 18:42:29 +0200

    small refactoring

--- UrGuide.WebApp/ClientApp/src/components/GuideRegistration.js
+++ UrGuide.WebApp/ClientApp/src/components/GuideRegistration.js
@@ -1,24 +1,18 @@
 ﻿import React, { Component } from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormControl from "@material-ui/core/FormControl";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
+import {
+    Button,
+    Link,
+    Box,
+    Container,
+    Step,
+    StepLabel,
+    Stepper
+} from "@material-ui/core";
+
 // import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel";
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
+
 import { Step1 } from "./Step1";
 import { Step2 } from "./Step2";
 import { Step3 } from "./Step3";
