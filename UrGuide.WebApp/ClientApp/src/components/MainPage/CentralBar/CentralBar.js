d7f63b7087e425c468e8ed016a16633793080cf1
Ihor Molchan <molchanigor54@gmail.com>
2020-04-16 14:56:00 +0300

    reverted changes

--- UrGuide.WebApp/ClientApp/src/components/MainPage/CentralBar/CentralBar.js
+++ UrGuide.WebApp/ClientApp/src/components/MainPage/CentralBar/CentralBar.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+import UserContext from '../../../UserContext';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
@@ -7,15 +8,33 @@ import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import {red} from '@material-ui/core/colors';
 import ShareIcon from '@material-ui/icons/Share';
+import PhotoIcon from '@material-ui/icons/Photo';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import AddLocationIcon from '@material-ui/icons/AddLocation';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import Button from '@material-ui/core/Button';
 import CardMedia from '@material-ui/core/CardMedia';
 import TextField from '@material-ui/core/TextField';
 import ChatIcon from '@material-ui/icons/Chat';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import TagFacesIcon from '@material-ui/icons/TagFaces';
 import PropTypes from 'prop-types';
 import './CentralStyle.css';
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 import {withStyles} from '@material-ui/core/styles';
+import {SdCard} from '@material-ui/icons';
 import AddPhoto, {PhotoX} from './../../AddPhoto/AddPhoto';
 import AddPhotoContext from './../../AddPhoto/AddPhotoContext';
 
@@ -34,7 +53,6 @@ const styles = {
     color: 'white',
     height: 30,
     padding: '0 30px',
-    margin: 8,
   },
 };
 
@@ -51,6 +69,7 @@ ButtonInPosts.propTypes = {
 const ButtonP = withStyles (styles) (ButtonInPosts);
 
 export default function CentralBar () {
+  const user = useContext (UserContext);
   const uploadButton = React.createRef ();
 
   const [context, setContext] = React.useState ({
@@ -60,9 +79,8 @@ export default function CentralBar () {
 
   const update = files => {
     setContext ({files});
-    console.log ('was called');
   };
-
+  
   let posts = [
     {
       name: 'Excursion around Cherkassy',
@@ -88,23 +106,157 @@ export default function CentralBar () {
     },
   ];
 
+  function DatePicker () {
+    const [selectedDate, setSelectedDate] = React.useState (
+      new Date ('2020-04-13T14:48:54')
+    );
+
+    const handleDateChange = date => {
+      setSelectedDate (date);
+    };
+
+    return (
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify="space-around">
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="MM/dd/yyyy"
+            margin="normal"
+            label="Choose date"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            label="Choose time"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+          />
+        </Grid>
+      </MuiPickersUtilsProvider>
+    );
+  }
+
+  function ChipsArray () {
+    const [chipData, setChipData] = React.useState ([
+      {key: 0, label: 'Sport', checked: false},
+      {key: 1, label: 'Historical', checked: false},
+      {key: 2, label: 'Child', checked: false},
+      {key: 3, label: 'Nature', checked: false},
+      {key: 4, label: 'Other', checked: false},
+    ]);
+
+    return (
+      <Paper className="col-lg-12 d-flex justify-content-between">
+        {chipData.map (data => {
+          return (
+            <Chip
+              key={data.key}
+              color="primary"
+              variant={data.checked ? 'default' : 'outlined'}
+              label={data.label}
+              className="my-2"
+              onClick={() => {
+                let newChipsData = [...chipData];
+                newChipsData[data.key].checked = !newChipsData[data.key]
+                  .checked;
+                setChipData (newChipsData);
+              }}
+            />
+          );
+        })}
+      </Paper>
+    );
+  }
+
+  function ViewPost () {
+    const [ViewPostCreating, setViewPostCreating] = useState ('button');
+
+    if (ViewPostCreating === 'button') {
+      return (
+        <div
+          onClick={() => setViewPostCreating ('post')}
+          className={`col-lg-12 p-3 mb-3 bg-white rounded new-post-card`}
+        >
+          <div className="new-post-btn">
+            <span>Want to write a new post ?</span>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className={`col-lg-12 p-3 mb-3 bg-white rounded shadow-lg bg-white rounded`}
+        >
+          <div className="col-lg-12 d-flex justify-content-between">
+            <Typography variant="h5">Create your post!</Typography>
+            <ButtonP
+              onClick={() => setViewPostCreating ('button')}
+              variant="outlined"
+              color="blue"
+            >
+              X
+            </ButtonP>
+          </div>
+          <div className="col-lg-12 d-flex justify-content-start my-4">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            <Typography className="m-0 px-4" variant="h6">
+              {user.username}
+            </Typography>
+          </div>
+          <div className="col-lg-12">
+            <TextField fullWidth label="Name" variant="outlined" />
+            <TextField
+              fullWidth
+              className="my-2"
+              label="Short description"
+              multiline
+              rows="6"
+              variant="outlined"
+            />
+            <TextField label="Price" variant="outlined" />
+            <DatePicker />
+            <ChipsArray />
+            <AddPhotoContext.Provider value={context}>
+              <PhotoX />
+              <AddPhoto fileInput={uploadButton} update={update} />
+            </AddPhotoContext.Provider>
+          </div>
+          <div className="col-lg-12 my-2">
+            <ButtonGroup
+              fullWidth
+              size="large"
+              color="primary"
+              aria-label="large outlined primary button group"
+            >
+              <Button onClick={() => uploadButton.current.click ()}>
+                <PhotoIcon />
+              </Button>
+              <Button><VideoLibraryIcon /></Button>
+              <Button><AddLocationIcon /></Button>
+              <Button><MoreHorizIcon /></Button>
+            </ButtonGroup>
+          </div>
+          <div className="col-lg-12">
+            <ButtonP fullWidth variant="outlined" color="blue">Publish</ButtonP>
+          </div>
+        </div>
+      );
+    }
+  }
+
   return (
     <div className="col-12 col-sm-7 col-md-7 col-lg-6 col-xl-5 timeline">
       <div className="container">
-        <div className="p-3 mb-3 bg-white rounded new-post-card">
-          <div className="new-post-btn">
-            <span>Want to write a new post ?</span>
-            <button onClick={() => uploadButton.current.click ()}>
-              Upload
-            </button>
 
-          </div>
-        </div>
-        <AddPhotoContext.Provider value={context}>
-            <PhotoX />
-            <AddPhoto fileInput={uploadButton} update={update} />
-        </AddPhotoContext.Provider>
-       
+        <ViewPost />
         {posts.map ((post, i) => (
           <div key={i} className="p-3 mb-3 bg-white rounded post-card">
             <CardHeader
