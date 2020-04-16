37d67317690cbd7608f4317f6ece3a20ada7bfed
Stanislav <gamongara@gmail.com>
2020-04-16 13:48:14 +0300

    I added postCreatingUI and chips for choosing category

--- UrGuide.WebApp/ClientApp/src/components/MainPage/CentralBar/CentralBar.js
+++ UrGuide.WebApp/ClientApp/src/components/MainPage/CentralBar/CentralBar.js
@@ -20,8 +20,19 @@ import Button from '@material-ui/core/Button';
 import CardMedia from '@material-ui/core/CardMedia';
 import TextField from '@material-ui/core/TextField';
 import ChatIcon from '@material-ui/icons/Chat';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import TagFacesIcon from '@material-ui/icons/TagFaces';
 import PropTypes from 'prop-types';
 import './CentralStyle.css';
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 import { withStyles } from '@material-ui/core/styles';
 import { SdCard } from '@material-ui/icons';
 
@@ -84,6 +95,75 @@ export default function CentralBar() {
     ]
 
 
+    function DatePicker() {
+
+        const [selectedDate, setSelectedDate] = React.useState(new Date('2020-04-13T14:48:54'));
+      
+        const handleDateChange = (date) => {
+          setSelectedDate(date);
+        };
+      
+        return (
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container justify="space-around">
+              <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                label="Choose date"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+              <KeyboardTimePicker
+                margin="normal"
+                label="Choose time"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}
+              />
+            </Grid>
+          </MuiPickersUtilsProvider>
+        );
+    }
+
+
+    function ChipsArray() {
+      const [chipData, setChipData] = React.useState([
+        { key: 0, label: 'Sport', checked:false },
+        { key: 1, label: 'Historical', checked:false },
+        { key: 2, label: 'Child', checked:false },
+        { key: 3, label: 'Nature', checked:false },
+        { key: 4, label: 'Other', checked:false },
+      ]);
+    
+      return (
+        <Paper className="col-lg-12 d-flex justify-content-between">
+          {chipData.map((data) => {
+            return (
+              <Chip
+                key={data.key}
+                color="primary"
+                variant={data.checked ? 'default' : 'outlined' }
+                label={data.label}
+                className='my-2'
+                onClick={() => {
+                  let newChipsData = [...chipData]
+                  newChipsData[data.key].checked = !newChipsData[data.key].checked;
+                  setChipData(newChipsData)
+                }}
+              />
+            );
+          })}
+        </Paper>
+      );
+    }
+
 
     function ViewPost() {
 
@@ -99,35 +179,31 @@ export default function CentralBar() {
         } else {
             return(
             <div className={`col-lg-12 p-3 mb-3 bg-white rounded shadow-lg bg-white rounded`}>
-                <div className="col-lg-12 row d-flex justify-content-between">
+                <div className="col-lg-12 d-flex justify-content-between">
                     <Typography variant="h5">Create your post!</Typography>
                     <ButtonP onClick={() => setViewPostCreating('button')} variant="outlined" color="blue">X</ButtonP>
                 </div>
-                <div className="col-lg-12 row my-4">
+                <div className="col-lg-12 d-flex justify-content-start my-4">
                     <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                    <Typography className="mx-4" variant="h6">{user.username}</Typography>
+                    <Typography className="m-0 px-4" variant="h6">{user.username}</Typography>
                 </div>
-                <div className="col-lg-12 row">
-                    <TextField
-                        fullWidth
-                        label="Short description"
-                        multiline
-                        rows="6"
-                        variant="outlined"
-                />
+                <div className="col-lg-12">
+                    <TextField fullWidth label="Name" variant="outlined" />
+                    <TextField fullWidth className="my-2" label="Short description" multiline rows="6" variant="outlined"/>
+                    <TextField label="Price" variant="outlined" />
+                    <DatePicker />
+                    <ChipsArray />
                 </div>
-                <div className="col-lg-12 row my-2">
+                <div className="col-lg-12 my-2">
                     <ButtonGroup fullWidth size="large" color="primary" aria-label="large outlined primary button group">
                         <Button><PhotoIcon /></Button>
                         <Button><VideoLibraryIcon /></Button>
-                        <Button><PersonAddIcon /></Button>
                         <Button><AddLocationIcon /></Button>
-                        <Button><ShareIcon /></Button>
                         <Button><MoreHorizIcon /></Button>
                     </ButtonGroup>
                 </div>
-                <div className="col-lg-12 row">
-                    <ButtonP fullWidth variant="outlined" color="blue" href="#outlined-buttons">Publish</ButtonP>
+                <div className="col-lg-12">
+                    <ButtonP fullWidth variant="outlined" color="blue">Publish</ButtonP>
                 </div>
             </div>)
         }
