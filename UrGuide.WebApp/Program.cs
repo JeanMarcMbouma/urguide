bb2cedcf71b990f2cd9a1514f5696bc4c5895a4a
Jean Marc Mbouma <jeanmarcm@instem.com>
2020-05-19 09:31:15 +0300

    Added nlogs, remove the serviceWorker

--- UrGuide.WebApp/Program.cs
+++ UrGuide.WebApp/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
+using System;
+using NLog.Web;
 
 namespace UrGuide.WebApp
 {
@@ -7,7 +10,23 @@ namespace UrGuide.WebApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                //NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -15,6 +34,12 @@ namespace UrGuide.WebApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+              .ConfigureLogging(logging =>
+              {
+                  logging.ClearProviders();
+                  logging.SetMinimumLevel(LogLevel.Trace);
+              })
+              .UseNLog();
     }
 }
