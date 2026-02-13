6d930662ff0e488bc5df1040b99d2cf34255078e
Jean Marc Mbouma <jm@minetechservices.com>
2026-02-13 18:37:37 +0000

    fix: update UserId length to 128 characters across multiple tables in the database schema

--- UrGuide.Data/Migrations/20260213183354_FixUserIdLength.Designer.cs
+++ UrGuide.Data/Migrations/20260213183354_FixUserIdLength.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using UrGuide.Data;
@@ -12,9 +13,11 @@ using UrGuide.Data;
 namespace UrGuide.Data.Migrations
 {
     [DbContext(typeof(UrGuideContext))]
-    partial class UrGuideContextModelSnapshot : ModelSnapshot
+    [Migration("20260213183354_FixUserIdLength")]
+    partial class FixUserIdLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UrGuide.Data/Migrations/20260213183354_FixUserIdLength.cs
@@ -0,0 +1,322 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrGuide.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUserIdLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Users",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "User_Notifications",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_User_Notification_Users",
+                schema: "ug",
+                table: "User_Notifications",
+                type: "nvarchar(128)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "User_Images",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "User_Feedback",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_User_Feedback_Users",
+                schema: "ug",
+                table: "User_Feedback",
+                type: "nvarchar(128)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "User_Attributes",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RequesterId",
+                schema: "ug",
+                table: "tour_requests",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                schema: "ug",
+                table: "tour_booking",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Seat_Reservations",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Posts",
+                type: "nvarchar(128)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Post_UserReactions",
+                type: "nvarchar(128)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_Post_Feedback_Users",
+                schema: "ug",
+                table: "Post_Feedback",
+                type: "nvarchar(128)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_Post_Bids_History_Users",
+                schema: "ug",
+                table: "Post_Bids_History",
+                type: "nvarchar(128)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_Post_Bids_Users",
+                schema: "ug",
+                table: "Post_Bids",
+                type: "nvarchar(128)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Image_Catalogs",
+                type: "nvarchar(128)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "User_Notifications",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_User_Notification_Users",
+                schema: "ug",
+                table: "User_Notifications",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "User_Images",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "User_Feedback",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_User_Feedback_Users",
+                schema: "ug",
+                table: "User_Feedback",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "User_Attributes",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RequesterId",
+                schema: "ug",
+                table: "tour_requests",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                schema: "ug",
+                table: "tour_booking",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Seat_Reservations",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Posts",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Post_UserReactions",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_Post_Feedback_Users",
+                schema: "ug",
+                table: "Post_Feedback",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_Post_Bids_History_Users",
+                schema: "ug",
+                table: "Post_Bids_History",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FK_Post_Bids_Users",
+                schema: "ug",
+                table: "Post_Bids",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "ug",
+                table: "Image_Catalogs",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldNullable: true);
+        }
+    }
+}
